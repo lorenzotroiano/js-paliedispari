547b04ad126f478587522f76174d9cd2112bcbc6
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-08 16:19:00 +0200

    eseguita funzione per la somma con condizioni

--- altroEsercizio/jsNum/numeri.js
+++ altroEsercizio/jsNum/numeri.js
@@ -8,19 +8,57 @@ console.log(sceltaParODisp);
 
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-let numRandom = Math.floor(Math.random() * 5) + 1;
-console.log(numRandom);
+// let numRandom = Math.floor(Math.random() * 5) + 1;
+
+
+let numRandomPc = numRandom(1, 5);
+console.log(numRandomPc);
+
+
+
+
+    function numRandom(min, max) {
+
+        return Math.floor(Math.random() * (max - min) ) + min;
+
+      }
+
+
+
+
+
 
 
 // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.l
+/*
+const sommaNum = numRichiesto + numRandomPc;
+console.log(sommaNum); */
 
-const sommaNum = numRichiesto + numRandom;
-console.log(sommaNum);
-
+/*
 if (sommaNum % 2 === 0 && sceltaParODisp === "pari" || sommaNum % 2 !== 0 && sceltaParODisp === "dispari") {
     console.log("Complimenti User, hai vinto!");
 } else if (sceltaParODisp !== "pari" && sceltaParODisp !== "dispari") {
     console.log("Il gioco non può funzionare, inserisci correttamente pari o dispari!");
 } else {
     console.log("Ha vinto il computer, mi spiace User, ritenta!");
+} */
+
+const somma = numPariODisp(numRichiesto,numRandomPc);
+console.log(somma);
+
+
+function numPariODisp(num1, num2) {
+
+    const sommaNum = num1 + num2;
+
+    if (sommaNum % 2 === 0 && sceltaParODisp === "pari" || sommaNum % 2 !== 0 && sceltaParODisp === "dispari") {
+        console.log("Complimenti User, hai vinto!");
+    } else if (sceltaParODisp !== "pari" && sceltaParODisp !== "dispari") {
+        console.log("Il gioco non può funzionare, inserisci correttamente pari o dispari!");
+    } else {
+        console.log("Ha vinto il computer, mi spiace User, ritenta!");
+    }
+
+    return sommaNum;
+
 }
